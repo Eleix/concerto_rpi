4517d1709bfdfe977be751d95ed10cb89c3d1d03
Francis Booth <rizen@atlum.net>
2015-09-04 21:22:27 -0400

    Update piinstaller.sh

--- piinstaller.sh
+++ piinstaller.sh
@@ -57,7 +57,7 @@ sleep 2
 rm -rf ./.tmp
 
 #Moves the refresh script into the home folder and adds an executable bit so that the cronjob can execute it properly.
-mv ~pi/concerto_pi/refresh.sh ../
+mv ~pi/concerto_pi/refresh.sh ~pi/refresh.sh
 chmod +x ~pi/refresh.sh 
 
 #Script is finished, System is rebooted at the end of the script.
