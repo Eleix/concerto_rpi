f8688e07d58c848da3cc20a6901ce7c2f437cb78
Francis Booth <rizen@atlum.net>
2016-03-11 17:07:18 -0500

    Added manual pull of Chromium
    Latest version of Raspbian does not contain an updated version of the Chromium browser and now has to pull it from Ubuntu's maintainers.

--- piinstaller.sh
+++ piinstaller.sh
@@ -21,7 +21,17 @@ fi
 
 #Checks for and updates the system before installing the necessary tools.
 apt-get update && apt-get upgrade -y
-apt-get install git screen chromium-browser ntpdate vim xdotool unattended-upgrades -y
+apt-get install git screen ntpdate vim xdotool unattended-upgrades -y
+
+wget http://ftp.us.debian.org/debian/pool/main/libg/libgcrypt11/libgcrypt11_1.5.0-5+deb7u3_armhf.deb
+wget http://launchpadlibrarian.net/218525709/chromium-browser_45.0.2454.85-0ubuntu0.14.04.1.1097_armhf.deb
+wget http://launchpadlibrarian.net/218525711/chromium-codecs-ffmpeg-extra_45.0.2454.85-0ubuntu0.14.04.1.1097_armhf.deb
+sudo dpkg -i libgcrypt11_1.5.0-5+deb7u3_armhf.deb
+sudo dpkg -i chromium-codecs-ffmpeg-extra_45.0.2454.85-0ubuntu0.14.04.1.1097_armhf.deb
+sudo dpkg -i chromium-browser_45.0.2454.85-0ubuntu0.14.04.1.1097_armhf.deb
+rm -rf chromium-browser_45.0.2454.85-0ubuntu0.14.04.1.1097_armhf.deb
+rm -rf chromium-codecs-ffmpeg-extra_45.0.2454.85-0ubuntu0.14.04.1.1097_armhf.deb
+rm -rf libgcrypt11_1.5.0-5+deb7u3_armhf.deb
 
 #moves to the pi user's home directory and clones the concerto install script
 cd ~pi/
